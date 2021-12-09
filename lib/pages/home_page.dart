884c2fb9be5bac522b9e3b7c42e1c3fa05884c88
anangfatkur <anangfatkurrr@gmail.com>
2021-12-09 09:30:01 +0700

    penggunaan fitur snackbar atau notif getx

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,7 +3,7 @@ import 'package:get/get.dart';
 import '../routes/name_routes.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({ Key? key }) : super(key: key);
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -12,7 +12,29 @@ class HomePage extends StatelessWidget {
         title: Text("HomePage"),
       ),
       body: Center(
-        child: ElevatedButton(onPressed: () => Get.toNamed(RouteName.productpage), child: Text("All Product >>")),
+        child: ElevatedButton(
+            onPressed: () => Get.toNamed(RouteName.productpage),
+            child: Text("All Product >>")),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Get.snackbar(
+            "hallo ini judul",
+            "hallo ini pesan",
+            animationDuration: Duration(milliseconds: 200),
+            duration: Duration(seconds: 2),
+            backgroundColor: Colors.green,
+            backgroundGradient: LinearGradient(
+              colors: [
+                Colors.yellow,
+                Colors.red,
+              ],
+            ),
+            borderColor: Colors.blue,
+            borderWidth: 4,
+          );
+        },
+        child: Icon(Icons.add),
       ),
     );
   }
